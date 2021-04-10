88c82ce2bbd9b59c2fc4a0129712ebf3b2c5ac1f
Tadej Glinšek <tadej.glinsek@gmail.com>
2021-04-11 00:15:42 +0200

    Dodal metode za postavljanje kamnov in preverjanje, če je matrika polna.

--- Gomoku/src/logika/Igra.java
+++ Gomoku/src/logika/Igra.java
@@ -1,5 +1,7 @@
 package logika;
 
+import splosno.Koordinati;
+
 public class Igra {
 	
 	private Matrika matrika;
@@ -7,14 +9,36 @@ public class Igra {
 	public Stanje trenutnoStanje;
 	
 	
-	public Igra () {
+	public Igra() {
 		this(15);  // klièe konstruktor s parametrom
 	}
 	
 	
-	public Igra (int stranica) {
+	public Igra(int stranica) {
 		this.matrika = new Matrika(stranica);
 		this.igralecNaPotezi = Igralec.BELI;
 		this.trenutnoStanje = Stanje.V_TEKU;
+	}
+	
+	
+	public int dimenzijaMatrike() {
+		return matrika.vrniDimenzije();
+	}
+	
+	
+	public boolean igraJePolna() {
+		return this.matrika.matrikaJePolna();
+	}
+	
+	
+	public boolean igraj(Koordinati k) {
+		if (matrika.vrniClen(k) == Polje.PRAZNO) {
+			matrika.zamenjajClen(k, igralecNaPotezi.barvaPoteze());
+			igralecNaPotezi = igralecNaPotezi.pridobiNasprotnika();
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 }
--- Gomoku/src/logika/Matrika.java
+++ Gomoku/src/logika/Matrika.java
@@ -8,18 +8,50 @@ public class Matrika {
 	private int stranica;
 	
 	
-	public Matrika (int stranica) {
+	public Matrika(int stranica) {
 		this.stranica = stranica;
 		this.matrika = new Polje[stranica][stranica];
 	}
 	
 	
-	public Polje vrniClen (Koordinati k) {
+	public int vrniDimenzije() {
+		return stranica;
+	}
+	
+	
+	public void izprazniMatriko() {
+		for (int i = 0; i < this.stranica; i++) {
+			for (int j = 0; j < this.stranica; j++) {
+				this.matrika[i][j] = Polje.PRAZNO;
+			}
+		}
+	}
+	
+	
+	public Polje[][] pridobiMatriko() {
+		return this.matrika;
+	}
+	
+	
+	public Polje vrniClen(Koordinati k) {
 		return this.matrika[k.getY()][k.getX()];
 	}
 	
 	
-	public void zamenjajClen (Koordinati k, Polje novClen) {
+	public void zamenjajClen(Koordinati k, Polje novClen) {
 		this.matrika[k.getY()][k.getX()] = novClen;
 	}
+	
+	
+	public boolean matrikaJePolna() {  // vrne true, èe nobeno polje ni prazno
+		boolean obstajaPraznoPolje = false;
+		for (int i = 0; i < this.stranica; i++) {
+			for (int j = 0; j < this.stranica; j++) {
+				if (this.matrika[i][j] == Polje.PRAZNO) {
+					obstajaPraznoPolje = true;
+				}
+			}
+		}
+		return !obstajaPraznoPolje;
+	} 
 }
